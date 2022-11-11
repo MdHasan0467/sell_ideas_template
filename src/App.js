7d2bdaba668742c48a36cd5811bc8be500d993c1
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2022-11-12 00:37:09 +0600

    text

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Bismillah</h1>
+      <p>Lets Start Now !</p>
     </div>
   );
 }
